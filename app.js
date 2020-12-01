bc18e0de6188afb0ba14e8019d8b65e5d3caf5b8
dennis <denniscassa@gmail.com>
2020-12-01 12:04:49 -0500

    migrado a ejs trbajo de la compañera

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var inicioRouter = require('./routes/inicio');
+var monitoreoRouter = require('./routes/monitoreo');
+var asistenciaRouter = require('./routes/asistencia');
+var conectadosRouter = require('./routes/conectados');
+var recorridoRouter = require('./routes/recorrido');
+var tiemposControlRouter = require('./routes/tiemposControl');
 
 var app = express();
 
@@ -25,6 +30,11 @@ app.use('/', loginRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/inicio', inicioRouter);
+app.use('/monitoreo', monitoreoRouter);
+app.use('/asistencia', asistenciaRouter);
+app.use('/conectados', conectadosRouter);
+app.use('/recorrido', recorridoRouter);
+app.use('/tiemposControl', tiemposControlRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/recorrido.js
+++ routes/recorrido.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('inicio/inicio');
+  res.render('consultas/recorrido');
 });
 
 module.exports = router;
